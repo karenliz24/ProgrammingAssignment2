829cda950c977b3b703c8c9b19ac917c9e943c63
karenliz24 <73143102+karenliz24@users.noreply.github.com>
2020-10-19 17:27:19 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,10 +6,13 @@
 makeCacheMatrix <- function(x = matrix()) {
 
 }
-
+x1 <- makeInverse(matrix(c(1,2,3,4),2,2))
+x1$getsolve() #Inverse not computed yet
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+x1$set(x1$getsolve()) #Setting the function call to be the computed inverse
+cachesolve(x1) #Inverse of the inverse is the original matrix
